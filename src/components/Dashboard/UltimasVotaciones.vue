3c43b8a399bbfd5d00c399736eeb14dc81974113
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-13 21:24:49 -0400

    created ultimasvotaciones component , imported y agregado a dashboard

---
+++ src/components/Dashboard/UltimasVotaciones.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ultimasvotaciones">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name:'UltimasVotaciones',
+    components:{},
+    data:function(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,17 +4,21 @@
             <div class="row m-0">
                 <SumarryVotes></SumarryVotes>
             </div>
+            <div class="row m-0">
+                <UltimasVotaciones></UltimasVotaciones>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import SumarryVotes from '@/components/Dashboard/SumarryVotes.vue'
-
+import UltimasVotaciones from '@/components/Dashboard/UltimasVotaciones.vue'
 export default {
     name:"Dashboard",
     components:{
-        SumarryVotes
+        SumarryVotes,
+        UltimasVotaciones
     },
     data:function(){
         return{
