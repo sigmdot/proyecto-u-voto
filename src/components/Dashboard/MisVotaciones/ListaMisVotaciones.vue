4ce80c554607832b7f3ff953e75cfd7f94828399
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-13 22:22:02 -0400

    creada lista para mostrar mis votaciones

---
+++ src/components/Dashboard/MisVotaciones/ListaMisVotaciones.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="listamisvotaciones">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:'ListaMisVotaciones',
+    components:{
+
+    },
+    data:function(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
