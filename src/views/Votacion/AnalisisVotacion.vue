cbbf06106bb29ba8c185bc05ff00c99363dd8cc3
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-14 13:48:19 -0400

    Creada vista analisis votacion

---
+++ src/views/Votacion/AnalisisVotacion.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="analisisvotaciones">
+     
+ </div>
+</template>
+<script>
+export default {
+    name:'AnalisisVotacion',
+    components:{
+
+    },
+    data:function(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
