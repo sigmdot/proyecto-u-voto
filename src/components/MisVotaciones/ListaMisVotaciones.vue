a1c031c486d35ca5d3c33be06ba50a137207e418
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-14 13:21:50 -0400

    actualizado componentes

--- src/components/MisVotaciones/ListaMisVotaciones.vue
+++ src/components/MisVotaciones/ListaMisVotaciones.vue
@@ -12,7 +12,16 @@
           <VotacionItemPC></VotacionItemPC>
           <VotacionItemPC></VotacionItemPC>
         </div>
+        
       </div>
+      <div class="is-hidden-pc">
+          <h4>Votaciones</h4>
+          <div class="row m-0">
+            <div class="col-md-12 col-sm 12 col-12 shadow">
+              jajaaj
+            </div>
+          </div>
+        </div>
     
   </div>
 </template>
