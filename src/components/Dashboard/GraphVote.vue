caa56782a1ef0f1a5a9320b15baf3bbe18adc6e4
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-13 21:17:27 -0400

    installed vuechartjs, add bargraph.vue, updae graphvote comp

--- src/components/Dashboard/GraphVote.vue
+++ src/components/Dashboard/GraphVote.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="graphvote">
       <div class="container-fluid">
-          
+          <BarGraph></BarGraph>
       </div>
     
   </div>
 </template>
 
 <script>
+import BarGraph from './BarGraph.vue'
 export default {
   name: "GraphVote",
-  components: {},
+  components: {
+      BarGraph
+  },
   data: function() {
     return {};
   }
