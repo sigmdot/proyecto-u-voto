cda157b21aa8f2a87ba852f476db89e837494d09
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-14 17:31:04 -0400

    creado preguntarespuesta ( el componente que almacena la pregunta )
    y los resultados de esta

---
+++ src/components/MisVotaciones/AnalisisVotacion/PreguntaRespuestas.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="PreguntaRespuesta bordered col-12 p-0">
+      <h5>{{preguntaNombre}}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'PreguntaRespuesta',
+    components:{
+
+    },
+    data:function(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
