88cb026e845cc306899e4a27cdf7e4884c23c99c
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-25 16:22:22 -0400

    Login actualizado

--- src/components/Login/FormularioLogin.vue
+++ src/components/Login/FormularioLogin.vue
@@ -1,24 +1,69 @@
 <template>
-  <form>
+  <form @submit="checkForm">
     <div class="form-group">
       <label for="email">Correo electronico:</label>
-      <input type="email" class="form-control" id="email" aria-describedby="email" />
+      <input type="email" class="form-control" id="email" aria-describedby="email" v-model="correo" />
+      {{correo}}
     </div>
     <div class="form-group">
       <label for="password">Contraseña:</label>
-      <input type="password" class="form-control" id="password" />
+      <input type="password" class="form-control" id="password" v-model="pass" />
+      {{pass}}
     </div>
-    <div class="form-group form-check">
+    <!-- <div class="form-group form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1" />
       <label class="form-check-label" for="exampleCheck1">Recuerdáme!</label>
-    </div>
+    </div>-->
     <button type="submit" class="btn btn-primary">Entrar</button>
   </form>
 </template>
 
 <script>
 export default {
-    name:'LoginFormulario'
+  name: "LoginFormulario",
+  data: function() {
+    return {
+      correo: null,
+      pass: null
+    };
+  },
+  methods: {
+    checkForm:function(e) {
+      const h = this.$createElement;
+      const vNodesTitle = h(
+        "div",
+        { class: ["d-flex", "flex-grow-1", "align-items-baseline", "mr-2"] },
+        [h("strong", { class: "mr-2" }, "Atención")]
+      );
+
+      if (this.correo && this.pass) {
+        return true;
+      }
+      if (!this.correo) {
+        const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
+          h("strong"),
+          ` El correo es necesario `
+        ]);
+        this.$bvToast.toast([vNodesMsg], {
+          title: [vNodesTitle],
+          solid: true,
+          variant: "warning"
+        });
+      }
+      if(!this.pass){
+        const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
+          h("strong"),
+          `La contraseña es obligatoria `
+        ]);
+        this.$bvToast.toast([vNodesMsg], {
+          title: [vNodesTitle],
+          solid: true,
+          variant: "warning"
+        });
+      }
+      e.preventDefault();
+    }
+  }
 };
 </script>
 
