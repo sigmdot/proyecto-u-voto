1c54c8401e4d8e811b6f90f7247373c0376199cc
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-26 20:53:08 -0400

    creada conexión firebase,ingresomodal

---
+++ api/service.js
@@ -0,0 +1,16 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDLP2Ppcc-0SY8XmBXVXfuI0N_3s-Un1Jw",
+    authDomain: "proyecto-u-votos.firebaseapp.com",
+    databaseURL: "https://proyecto-u-votos.firebaseio.com",
+    projectId: "proyecto-u-votos",
+    storageBucket: "proyecto-u-votos.appspot.com",
+    messagingSenderId: "625887676932",
+    appId: "1:625887676932:web:dd57c9b5e49a8f620d0351",
+    measurementId: "G-NY98B2GCFP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
--- src/components/IngresoVotacionModal.vue
+++ src/components/IngresoVotacionModal.vue
@@ -2,10 +2,7 @@
   <b-modal id="modalvotacion" title="Ingresa a una aplicación" class="container-fluid">
     <div class="row m-0">
         <div class="col p-0">
-            <button type="button" class="btn btn-outline-primary"> Leer código QR </button>
-        </div>
-        <div class="col p-0">
-            <button type="button" class="btn btn-outline-dark"> Ingresar por link</button>
+            <button type="button" class="btn btn-outline-dark w-100"> Ingresar por link</button>
         </div>
     </div>
   </b-modal>
