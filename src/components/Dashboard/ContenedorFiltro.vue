aa389198356fbd22fa4c18eecd48dec6d53a9a9c
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-27 17:20:15 -0400

    actualizado contenedor filtro, tarjetas ultima votacion

--- src/components/Dashboard/ContenedorFiltro.vue
+++ src/components/Dashboard/ContenedorFiltro.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="col-12 p-2 row m-0 border contenedor">
         <TarjetaVotacionUltima></TarjetaVotacionUltima>
-        
+        <TarjetaVotacionUltima></TarjetaVotacionUltima>
+        <TarjetaVotacionUltima></TarjetaVotacionUltima>
   </div>
 </template>
 
--- src/components/Dashboard/TarjetaVotacionUltima.vue
+++ src/components/Dashboard/TarjetaVotacionUltima.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="col-12 col-lg-4 p-0 border row m-0 justify-content-center">
+  <div class="col-12 col-lg-4 p-0 row m-0 justify-content-center">
           <div class="col-10 p-0 row m-0">
             <div class="col-12 p-1 text-center bgheader text-white rounded border">
-              <h5 class="m-0"> Votacion ultima </h5>
+              <h2 class="m-0"> Votacion ultima </h2>
+            </div>
+            <div class="col-12 p-2 row m-0 border">
+              <div class="col-12 p-0">
+                <h3 class="text-center"><strong>Quien debe ser el pan</strong></h3>
+              </div>
+              <div class="col-12 p-0">
+                <h4 class="text-center col-12 p-0"> Hallulla </h4>
+              </div>
+              <div class="col-12 p-0">
+                <h5 class="text-center">452424324 Votos</h5>
+              </div>
             </div>
           </div>
         </div>
