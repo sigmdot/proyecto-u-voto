d5d277555d75e9bdcb93a158d5f793163b78a69c
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-07-15 01:29:51 -0400

    cambiado formulario registro

--- src/components/CrearVotacion/FormularioCreacion.vue
+++ src/components/CrearVotacion/FormularioCreacion.vue
@@ -1,40 +1,68 @@
 <template>
   <div class="formulariocreacion col-12 col-lg-10 p-3 border">
     <form @submit="checkForm">
-      <div class="form-group">
-        <label for="nombrevotacion">Nombre votación:</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">Nombre votación</span>
+        </div>
         <input
           type="text"
-          class="form-control"
+          class="form-control w-80"
+          style="width:82%"
           id="nombrevotacion"
           aria-describedby="nombrevotacionhelp"
           v-model="nombrevotacion"
         />
-        <small
-          id="emailHelp"
-          class="form-text text-muted"
-        >Ejemplo : Elección panes estudiantiles 2020</small>
       </div>
-
-      <!-- <div class="form-group">
-        <label for="exampleFormControlSelect1" >Tipo de elección:</label>
-        <select class="form-control" id="exampleFormControlSelect1" v-model="tipo">
-          <option value="private">Privada</option>
-          <option value="public">Pública</option>
-        </select>
-      </div> -->
+      <h4>Duración votación</h4>
+      <div class="form-group w-100">
+        <div class="col-12 p-0 row m-0 mb-3 w-100">
+          <b-form-datepicker v-model="fechaInicio" :min="min"  locale="es" class="col-5 p-0 w-100"></b-form-datepicker>
+          <div class="input-group-prepend col-7 p-0 pl-2">
+            <span class="input-group-text" id="basic-addon1">Hora inicio</span>
+            <input
+              type="text"
+              class="form-control w-100"
+              placeholder="Hora 00:00"
+              v-model="horaI"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+        </div>
+        <div class="col-12 p-0 row m-0 mt-1">
+          <b-form-datepicker v-model="fechaTermino" :min="min"  locale="es" class="col-5 p-0"></b-form-datepicker>
+          <div class="input-group-prepend col-7 p-0 pl-2">
+            <span class="input-group-text" id="basic-addon1" style="min-width:102.28px">Hora final</span>
+            <input
+              type="text"
+              class="form-control w-100"
+              placeholder="Hora 00:00"
+              v-model="horaT"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+        </div>
+      </div>
       <h4>Preguntas</h4>
       <div class="row m-0">
         <div class="btn btn-secondary col-4 col-md-1 col-lg-1 p-1 mb-3" v-b-modal.my-modal>
           <b-icon icon="plus-circle-fill"></b-icon>
         </div>
         <div class="col p-0 pl-1">
-            <p>Agregar una pregunta</p>
+          <p>Agregar una pregunta</p>
         </div>
       </div>
 
-      <div v-for="(pregunta,i) in preguntas" class="form-group border rounded p-3 bg-light" :key="i">
-        <label> <strong> {{pregunta.nombre}} </strong> </label>
+      <div
+        v-for="(pregunta,i) in preguntas"
+        class="form-group border rounded p-3 bg-light"
+        :key="i"
+      >
+        <label>
+          <strong>{{pregunta.nombre}}</strong>
+        </label>
         <div
           v-for="(respuesta,j) in pregunta.respuestas"
           :key="j"
@@ -47,9 +75,12 @@
             class="custom-control-input"
             disabled
           />
-          <label class="custom-control-label" for="customRadio1"> {{respuesta}}</label>
+          <label class="custom-control-label" for="customRadio1">{{respuesta}}</label>
         </div>
-        <div class="btn btn-outline-danger w-100 mt-3 w-100" @click="borrarpregunta(i)">Borrar pregunta</div>
+        <div
+          class="btn btn-outline-danger w-100 mt-3 w-100"
+          @click="borrarpregunta(i)"
+        >Borrar pregunta</div>
       </div>
       <button type="submit" class="btn btn-primary w-100" @click="logCrear">Crear votación</button>
     </form>
@@ -66,17 +97,27 @@ export default {
     ModalAgregar
   },
   data: function() {
+    const now = new Date()
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+      // 15th two months prior
+      const minDate = new Date(today)
+      minDate.setMonth(minDate.getMonth())
     return {
       preguntas: [],
-      tipo:'privada',
-      nombrevotacion:null
+      tipo: "privada",
+      nombrevotacion: null,
+      fechaInicio:'',
+      fechaTermino:'',
+      min:minDate,
+      horaI:null,
+      horaT:null
     };
   },
   methods: {
-    logCrear(){
-      console.log(this.preguntas)
-      console.log(this.tipo)
-      console.log(this.nombrevotacion)
+    logCrear() {
+      console.log(this.preguntas);
+      console.log(this.tipo);
+      console.log(this.nombrevotacion);
     },
     showModal() {
       this.$refs["my-modal"].show();
@@ -85,8 +126,8 @@ export default {
       this.$refs["my-modal"].hide();
     },
     checkForm(e) {
-      if(this.nombrevotacion && (this.preguntas.length != 0) && this.tipo){
-        this.$emit('votacion',this.nombrevotacion,this.tipo,this.preguntas);
+      if (this.nombrevotacion && this.preguntas.length != 0 && this.tipo) {
+        this.$emit("votacion", this.nombrevotacion, this.tipo, this.preguntas);
         this.nombrevotacion = null;
         this.preguntas = [];
         // this.tipo = null;
@@ -100,8 +141,8 @@ export default {
       };
       this.preguntas.push(pregunta);
     },
-    borrarpregunta(index){
-      this.preguntas.splice(index,1);
+    borrarpregunta(index) {
+      this.preguntas.splice(index, 1);
     }
   }
 };
--- src/components/Registro/Formulario.vue
+++ src/components/Registro/Formulario.vue
@@ -1,12 +1,12 @@
 <template>
-  <form class="formularioregistro" @submit="checkForm">
+  <form class="formularioregistro" @submit.prevent="checkForm">
     <div class="form-group">
       <label for="Nombreyapellido">Nombre:</label>
       <input
         type="text"
         class="form-control"
-        id="Nombreyapellido"
-        aria-describedby="Nombreyapellido"
+        id="Nombre"
+        aria-describedby="Nombr"
         v-model="nombres"
       />
     </div>
@@ -15,8 +15,8 @@
       <input
         type="text"
         class="form-control"
-        id="Nombreyapellido"
-        aria-describedby="Nombreyapellido"
+        id="Apellido"
+        aria-describedby="Apellido"
         v-model="apellidos"
       />
     </div>
@@ -30,9 +30,25 @@
         v-model="correo"
       />
     </div>
-    <div class="form-group">
+    <div class="form-group mb-1">
       <label for="password">Contraseña:</label>
       <input type="password" class="form-control" id="password" v-model="pass" />
+      <div class="progress mt-2" style="height:10px">
+        <div
+          v-if="pass"
+          class="progress-bar progress-bar-striped progress-bar-animated"
+          :class="{'bg-success': pass.length >= 8, 'bg-warning': pass.length <=7 && pass.length >= 6, 'bg-danger':pass.length <6 }"
+          role="progressbar"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          :style="{ 'width': (this.pass.length * 7) + '%' + ' !important'}"
+        ></div>
+      </div>
+      <div v-if="pass">
+        <span class="text-info" v-if="pass.length<5">Contraseña débil</span>
+        <span class="text-info" v-if="pass.length>5 && pass.length<=7">Contraseña media segura</span>
+        <span class="text-info" v-if="pass.length>7">Contraseña fuerte</span>
+      </div>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Re-ingresar contraseña:</label>
@@ -40,15 +56,44 @@
     </div>
     <div class="form-group">
       <label for="Rut">RUT:</label>
-      <input type="text" class="form-control" id="Rut" aria-describedby="rut" v-model="rut" />
+      <input
+        type="text"
+        class="form-control"
+        id="Rut"
+        aria-describedby="rut"
+        v-model="rut"
+        @change="validarRut"
+      />
       <small class="text-info">Ejemplo = 12345678-9</small>
       {{rut}}
     </div>
-    <button type="submit" class="btn btn-primary">¡Registrarme!</button>
+    <div class="form-group row m-0">
+      <label for="numerodoc" class="col-12 p-0">Numero documento:</label>
+      <input
+        type="text"
+        class="form-control col-11"
+        id="numeroDoc"
+        aria-describedby="numeroDoc"
+        v-model="ndoc"
+        @change="validarRut"
+      />
+      <div class="col p-0 text-center">
+        <div class="spinner-grow text-primary" role="status" v-if="load">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+      <small class="text-info col-12 p-0">Ejemplo = 25525525525</small>
+      {{ndoc}}
+    </div>
+    <div class="form-group text-right">
+      <button type="submit" class="btn btn-primary" :disabled="!validRut">¡Registrarme!</button>
+    </div>
   </form>
 </template>
 
 <script>
+import { isValid } from "validate-rut-serie";
+
 export default {
   name: "FormularioRegistro",
   data: function() {
@@ -62,12 +107,22 @@ export default {
       boolpass: false,
       pass: null,
       validapass: null,
-      rut: null
+      rut: null,
+      validRut: false,
+      ndoc: null,
+      load: false
     };
   },
   methods: {
     checkForm: function(e) {
-      console.log(this.nombres,this.apellidos,this.correo,this.pass,this.rut)
+      console.log(
+        this.nombres,
+        this.apellidos,
+        this.correo,
+        this.pass,
+        this.rut,
+        this.validRut
+      );
       const h = this.$createElement;
       const vNodesTitle = h(
         "div",
@@ -75,9 +130,8 @@ export default {
         [h("strong", { class: "mr-2" }, "Atención")]
       );
 
-      
       this.boolcorreo = true;
-         
+
       if (this.pass) {
         if (this.validapass) {
           if (this.pass.localeCompare(this.validapass) === 0) {
@@ -85,7 +139,6 @@ export default {
           }
         }
       }
-
       if (
         this.nombres &&
         this.apellidos &&
@@ -93,9 +146,17 @@ export default {
         this.boolcorreo &&
         this.pass &&
         this.boolpass &&
-        this.rut
+        this.rut &&
+        this.validRut
       ) {
-        this.$emit('datosRegistro',this.nombres,this.apellidos,this.correo,this.pass,this.rut);
+        this.$emit(
+          "datosRegistro",
+          this.nombres,
+          this.apellidos,
+          this.correo,
+          this.pass,
+          this.rut
+        );
       }
       this.errors = [];
       if (!this.nombres) {
@@ -131,7 +192,7 @@ export default {
           variant: "warning"
         });
       }
-  
+
       if (!this.boolcorreo) {
         const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
           h("strong"),
@@ -143,19 +204,26 @@ export default {
           variant: "warning"
         });
       }
-      if (this.pass.length < 6 || this.pass.length > 15  || this.validapass.length < 6 || this.validapass.length >15 ) {
-        const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
-          h("strong"),
-          ` la contraseña debee tener entre 6 y 15 caracteres `
-        ]);
-        this.$bvToast.toast([vNodesMsg], {
-          title: [vNodesTitle],
-          solid: true,
-          variant: "warning"
-        });
+      if (this.pass) {
+        if (
+          this.pass.length < 6 ||
+          this.pass.length > 15 ||
+          this.validapass.length < 6 ||
+          this.validapass.length > 15
+        ) {
+          const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
+            h("strong"),
+            ` la contraseña debee tener entre 6 y 15 caracteres `
+          ]);
+          this.$bvToast.toast([vNodesMsg], {
+            title: [vNodesTitle],
+            solid: true,
+            variant: "warning"
+          });
+        }
       }
-      
-      if(!this.pass){
+
+      if (!this.pass) {
         const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
           h("strong"),
           ` La contraseña es obligatoria `
@@ -166,7 +234,7 @@ export default {
           variant: "warning"
         });
       }
-      if(!this.validapass){
+      if (!this.validapass) {
         const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
           h("strong"),
           ` La validación de contraseña es obligatorio `
@@ -176,33 +244,94 @@ export default {
           solid: true,
           variant: "warning"
         });
-      }else{
-        if(!this.boolpass){
+      } else {
+        if (!this.boolpass) {
           const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
-          h("strong"),
-          ` Contraseñas no coinciden `
-        ]);
-        this.$bvToast.toast([vNodesMsg], {
-          title: [vNodesTitle],
-          solid: true,
-          variant: "warning"
-        });
+            h("strong"),
+            ` Contraseñas no coinciden `
+          ]);
+          this.$bvToast.toast([vNodesMsg], {
+            title: [vNodesTitle],
+            solid: true,
+            variant: "warning"
+          });
         }
       }
-      if(!this.rut){
-        const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
-          h("strong"),
-          `Rut requerido`
-        ]);
-        this.$bvToast.toast([vNodesMsg], {
-          title: [vNodesTitle],
-          solid: true,
-          variant: "warning"
-        });
-        }
-      
+      if (!this.rut) {
+        alert("washo falta el rut");
+      }
 
       e.preventDefault();
+    },
+    async validarRut() {
+      if (this.rut && this.ndoc) {
+        this.load = true;
+        try {
+          const valid = await isValid({ rut: this.rut, serie: this.ndoc }).then(
+            e => {
+              this.load = false;
+              this.validRut = e;
+               const h = this.$createElement;
+                const vNodesTitle = h(
+                  "div",
+                  {
+                    class: [
+                      "d-flex",
+                      "flex-grow-1",
+                      "align-items-baseline",
+                      "mr-2"
+                    ]
+                  },
+                  [h("strong", { class: "mr-2" }, "Atención")]
+                );
+              if (e === false) {
+                const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
+                  h("strong"),
+                  ` Datos de documento incorrecto `
+                ]);
+                this.$bvToast.toast([vNodesMsg], {
+                  title: [vNodesTitle],
+                  solid: true,
+                  variant: "warning"
+                });
+              }
+              else{
+                const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
+                  h("strong"),
+                  ` Datos de documento validados `
+                ]);
+                this.$bvToast.toast([vNodesMsg], {
+                  title: [vNodesTitle],
+                  solid: true,
+                  variant: "warning"
+                });
+              }
+            }
+          );
+          console.log(valid);
+        } catch (error) {
+          const h = this.$createElement;
+          const vNodesTitle = h(
+            "div",
+            {
+              class: ["d-flex", "flex-grow-1", "align-items-baseline", "mr-2"]
+            },
+            [h("strong", { class: "mr-2" }, "Atención")]
+          );
+          const vNodesMsg = h("p", { class: ["text-center", "mb-0"] }, [
+            h("strong"),
+            ` Hubo un error `
+          ]);
+          this.$bvToast.toast([vNodesMsg], {
+            title: [vNodesTitle],
+            solid: true,
+            variant: "warning"
+          });
+          console.log("Hubo un error");
+        }
+      } else {
+        this.validRut = false;
+      }
     }
   }
 };
